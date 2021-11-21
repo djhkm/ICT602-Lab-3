093ee7ce0f1a3475829783d176c6708f5e1a28b1
Hakim Djhkm <hakimdjhkm@gmail.com>
2021-11-22 02:54:06 +0800

    lab 3

---
+++ ICT602_Lab3/app/src/main/java/com/example/ict602_lab3/DeleteBiodataActivity.java
@@ -0,0 +1,14 @@
+package com.example.ict602_lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteBiodataActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_delete_biodata);
+  }
+}
